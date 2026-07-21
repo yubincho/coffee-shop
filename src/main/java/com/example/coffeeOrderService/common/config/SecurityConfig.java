12c5cd2a190df821154a8100465f21700f6fde38
yubincho <yubincho9@gmail.com>
2026-07-22 01:33:37 +0900

    refactor: 시큐리티 설정을 SecurityConfig로 분리하고 CoffeeShopConfig 제거

--- src/main/java/com/example/coffeeOrderService/common/config/SecurityConfig.java
+++ src/main/java/com/example/coffeeOrderService/common/config/SecurityConfig.java
@@ -9,12 +9,7 @@ import com.example.coffeeOrderService.common.auth.refreshToken.RefreshTokenRepos
 import com.example.coffeeOrderService.common.auth.user.UserDetailService;
 import com.example.coffeeOrderService.model.user.RoleRepository;
 import com.example.coffeeOrderService.service.user.UserService;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,69 +27,48 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @RequiredArgsConstructor
 @Configuration
-public class CoffeeShopConfig {
-
-    @PersistenceContext
-    private EntityManager entityManager;
+public class SecurityConfig {
 
     private final UserDetailService userDetailsService;
     private final OAuth2UserCustomService oAuth2UserCustomService;
+
     @Lazy
     private final JwtProvider jwtProvider;
 
     private final RefreshTokenRepository refreshTokenRepository;
-
-    private UserService userService;
     private final RoleRepository roleRepository;
 
-    // UserService에 Setter 주입 적용
+    private UserService userService;
+
     @Autowired
     public void setUserService(@Lazy UserService userService) {
         this.userService = userService;
     }
 
-
     private static final List<String> SECURED_URLS =
 //            List.of("/api/v1/carts/**", "/api/v1/cartItems/**", "/api/v1/login", "/api/v1/user");
             List.of("/api/cart/**");
-
-//    String apiKey = "";
-//    String secretKey = "";
-//
-//    @Bean
-//    public IamportClient iamportClient() {
-//        return new IamportClient(apiKey, secretKey);
-//    }
-
-
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
-                .requestMatchers(new AntPathRequestMatcher("/static/**")) // static 폴더 무시
+                .requestMatchers(new AntPathRequestMatcher("/static/**"))
                 .requestMatchers(new AntPathRequestMatcher("/templates/**"))
-                .requestMatchers(new AntPathRequestMatcher("/resources/**")) // resources 폴더도 포함 가능
-                .requestMatchers(new AntPathRequestMatcher("/webjars/**"));  // Webjars 리소스 무시
+                .requestMatchers(new AntPathRequestMatcher("/resources/**"))
+                .requestMatchers(new AntPathRequestMatcher("/webjars/**"));
     }
 
     @Bean
@@ -113,21 +87,19 @@ public class CoffeeShopConfig {
                 refreshTokenRepository,
                 oAuth2AuthorizationRequestBasedOnCookieRepository(),
                 userService,
-                roleRepository //
+                roleRepository
         );
     }
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        return http.csrf(AbstractHttpConfigurer::disable) // 배포시 활성화 필요
-                .cors(withDefaults())  // CORS 필터 추가
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .cors(withDefaults())
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class)  // JWT 인증을 수행
+                .addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth.requestMatchers(SECURED_URLS.toArray(String[]::new)).authenticated()
                         .anyRequest().permitAll())
                 .oauth2Login(oauth2 -> oauth2
@@ -144,10 +116,6 @@ public class CoffeeShopConfig {
                 .build();
     }
 
-    /**
-     * authenticationManager 설정
-     * DaoAuthenticationProvider 가 사용자 인증을 처리
-     * */
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -155,6 +123,4 @@ public class CoffeeShopConfig {
         authProvider.setPasswordEncoder(bCryptPasswordEncoder);
         return new ProviderManager(authProvider);
     }
-
-
 }
